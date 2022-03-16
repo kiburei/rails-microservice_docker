40f8dd942d8e1ab24a30f0afb8ae2049c67c1521
kiburei <gooddeeds17@gmail.com>
2022-03-16 11:04:30 +0300

    tests pass

--- spec/models/fx_transaction_spec.rb
+++ spec/models/fx_transaction_spec.rb
@@ -2,14 +2,22 @@ require 'rails_helper'
 
 RSpec.describe FxTransaction, type: :model do
 
-  describe "create" do
-    context "post an fx trnasaction to the db" do
-      it "validates all required params are present" do
-        expect { FxTransaction.create! }.to raise_error(ActiveRecord::RecordInvalid)
-      end
-    end
+  before(:all) do
+    @fx_transaction = FxTransaction.create(customer_id: 1232, input_amount: 100.00, input_currency: "USD", output_amount: 12000, output_currency: "KSH", transaction_date: "01/03/2022")
   end
 
-  
+  # check that @fx_transaction is a valid instance of FxTransaction class, validates all params as well
+    it "checks that a fx_transaction can be created" do
+      expect(@fx_transaction).to be_valid
+    end
+
+    it "checks that a transaction can be found by id" do
+      expect(FxTransaction.find(@fx_transaction.id)).to eq(@fx_transaction)
+    end
+
+    it "text" do
+
+    end
+
 
 end
